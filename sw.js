b28e34c7af96e1c3fab33b7d7c68f41373ca8b76
Crystal-Wei-Tiantian <52390460+Crystal-Wei-Tiantian@users.noreply.github.com>
2026-05-14 20:45:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = 'my-app-v1';
+const CACHE_NAME = 'shuying-v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  'manifest.json'
+  '/my-app/',
+  '/my-app/index.html',
+  '/my-app/manifest.json',
+  '/my-app/icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
   );
 });
